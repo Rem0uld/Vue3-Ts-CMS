506bee86b482cd96546f1ccee309cccb99578a3b
Rem0uld <l644081641@gmail.com>
2022-03-19 11:37:35 +0800

    项目创建

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "/login",
+    component: () => import("@/view/login/login.vue")
+  },
+  {
+    path: "/main",
+    component: () => import("@/view/main/main.vue")
+  }
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHistory()
+});
+
+export default router;
